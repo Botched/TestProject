bc2c6ca47612f8cceeee12cf173e192bada220a8
Botched <mupham1@gmail.com>
2017-04-27 20:48:52 -0600

    Initial test project commit

---
+++ Git.Test/Git.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Git.Test
+{
+    /// <summary>
+    ///  This is a test class, it will contain tests for one business area.
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// This is a test method, it will test one unit of work exclusively.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
